bbdaa5621d40dd1647994b2051bf4c07d5d8f21d
FelipeSants08 <felipe_santana.01@hotmail.com>
2026-02-09 14:04:58 -0300

    adição de logs para controle no desenvolvimento

--- src/main/java/com/santana/dev/emprestimo/controller/CustumerController.java
+++ src/main/java/com/santana/dev/emprestimo/controller/CustumerController.java
@@ -4,8 +4,10 @@ import com.santana.dev.emprestimo.model.Customer;
 import com.santana.dev.emprestimo.model.CustomerLoan;
 import com.santana.dev.emprestimo.service.CustumerService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,6 +21,7 @@ public class CustumerController {
     }
 
     @PostMapping("/customer-loans")
+    @ResponseStatus(HttpStatus.OK)
     public CustomerLoan custumer(@RequestBody Customer custumer) {
         log.info("Vendo quais empréstimos tem disponível para o usuário: {}", custumer.getName());
         CustomerLoan loan = service.loans(custumer);
--- src/main/java/com/santana/dev/emprestimo/model/Loans.java
+++ src/main/java/com/santana/dev/emprestimo/model/Loans.java
@@ -2,6 +2,6 @@ package com.santana.dev.emprestimo.model;
 
 public record Loans(String type, Integer interestRate) {
     public Loans(FeesType loan) {
-        this(loan.toString(),  loan.getFees());
+        this(loan.toString(), loan.getFees());
     }
 }
