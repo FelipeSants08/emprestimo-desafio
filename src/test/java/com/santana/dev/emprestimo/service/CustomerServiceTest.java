c691f74d1bbc655e2a4ee8ee7481e8bb42a479c1
FelipeSants08 <felipe_santana.01@hotmail.com>
2026-02-27 10:29:55 -0300

    mudando testes para que funcionem depois de refatorar

--- src/test/java/com/santana/dev/emprestimo/service/CustomerServiceTest.java
+++ src/test/java/com/santana/dev/emprestimo/service/CustomerServiceTest.java
@@ -3,14 +3,32 @@ package com.santana.dev.emprestimo.service;
 import com.santana.dev.emprestimo.model.Customer;
 import com.santana.dev.emprestimo.model.CustomerLoan;
 import com.santana.dev.emprestimo.model.FeesType;
+import com.santana.dev.emprestimo.validation.CheckLoanConsigment;
+import com.santana.dev.emprestimo.validation.CheckLoanGuaranteed;
+import com.santana.dev.emprestimo.validation.CheckLoanPersonal;
+import com.santana.dev.emprestimo.validation.IValidationCheck;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class CustumerServiceTest {
+class CustomerServiceTest {
 
-    CustumerService custumerService = new CustumerService();
+    private CustomerService service;
+
+    @BeforeEach
+    void setup() {
+        List<IValidationCheck> validation =  List.of(
+                new CheckLoanPersonal(),
+                new CheckLoanGuaranteed(),
+                new CheckLoanConsigment()
+        );
+        service = new CustomerService(validation);
+    }
+
 
     @Test
     @DisplayName("Deve conceder empréstimo pessoal e garantia quando a renda for menor ou igual a 3000")
@@ -19,7 +37,7 @@ class CustumerServiceTest {
         Customer cliente = new Customer(25, "123", "Fulano", 3000.0, "RJ");
 
 
-        CustomerLoan resultado = custumerService.loans(cliente);
+        CustomerLoan resultado = service.loans(cliente);
 
 
         assertEquals(2, resultado.loans().size(), "A lista deveria conter 2 empréstimos");
@@ -34,7 +52,7 @@ class CustumerServiceTest {
     public void deveRetornarLoansConsignatoQuandoRendaForSuperiorA5000() {
         Customer customer = new Customer(25, "555555555", "Astrogildo", 5100.0, "SP");
 
-        CustomerLoan resultado = custumerService.loans(customer);
+        CustomerLoan resultado = service.loans(customer);
 
         assertEquals(1, resultado.loans().size());
         assertEquals(FeesType.CONSIGNMENT.toString(), resultado.loans().get(0).type());
@@ -46,7 +64,7 @@ class CustumerServiceTest {
 
         Customer customer = new Customer(25, "555555555", "Antrolopoulos", 5000.0, "SP");
 
-        CustomerLoan resultado = custumerService.loans(customer);
+        CustomerLoan resultado = service.loans(customer);
 
         assertEquals(3, resultado.loans().size());
         assertEquals(FeesType.PERSONAL.toString(), resultado.loans().get(0).type());
